2d87e711ee47b9e8a6196643e00332b7e074e60c
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-04-07 22:33:41 -0300

    add tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+export default {
+  content: [
+    "./components/**/*.vue",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
