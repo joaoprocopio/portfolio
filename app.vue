6d0bc1268848212106e671fa0be19329e0e04893
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-04-09 01:34:24 -0300

    eslint + prettier + typescript

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
+
+<script setup lang="ts">
+  let test: string[] = ["a", "a"]
+
+  console.log(test)
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['~/assets/styles/index.css'],
+  css: ["~/assets/styles/index.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
-      autoprefixer: {}
-    }
-  }
+      autoprefixer: {},
+    },
+  },
 })
