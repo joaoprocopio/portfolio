d6ef600f71bd68f4d4a7b41a055f5ae0adb483db
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-04-15 00:36:59 -0300

    melhora no seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,19 +4,30 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
   app: {
     head: {
-      title: "João",
-      titleTemplate: "%s - Portfolio",
+      title: "Engenheiro de software e resolvedor de problemas.",
+      titleTemplate: "João Procópio: %s",
       htmlAttrs: {
         lang: "pt-BR",
       },
       meta: [
         { charset: "utf-8" },
-        { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
-        { name: "viewport", content: "width=device-width, initial-scale=1.0" },
         {
+          hid: "viewport",
+          name: "viewport",
+          content: "width=device-width, initial-scale=1.0, minimum-scale=1.0",
+        },
+        {
+          hid: "google-site-verification",
           name: "google-site-verification",
           content: "zSPPjtXp_CJ-oYGfkizcdrePI0Vt8tvGefn2msmrC5w",
         },
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Eu sou um estudante de engenharia de software. Eu acredito que a tecnologia pode resolver problemas reais e estou sempre procurando maneiras inovadoras e eficazes para enfrentar esses desafios.",
+        },
+        { hid: "robots", name: "robots", content: "index, follow" },
       ],
       link: [
         {
@@ -42,6 +53,11 @@ export default defineNuxtConfig({
           rel: "prefetch",
           as: "style",
           href: "https://fonts.googleapis.com/css2?family=Cousine:wght@400;700&display=swap",
+        },
+        {
+          key: "canonical",
+          rel: "canonical",
+          href: "http://joaoprocopio.github.io/",
         },
       ],
     },
