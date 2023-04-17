8ec71e3e109a637639b9e2611b2e9cf32c3d2402
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-04-17 01:53:23 -0300

    indigo

--- error.vue
+++ error.vue
@@ -3,21 +3,21 @@
     <shared-container
       class="h-screen"
       content-class="h-full flex flex-col justify-center">
-      <h1 class="font-mono tracking-widest mb-1 text-neutral-700 uppercase">
+      <h1
+        class="ml-[2px] font-mono tracking-widest mb-2 text-indigo-700 uppercase">
         Desculpe
       </h1>
-      <h2 class="text-4xl font-semibold mb-2">Página não encontrada</h2>
+      <h2 class="text-4xl font-semibold mb-4">Página não encontrada</h2>
       <p class="mb-8">
         A página
-        <code
-          class="font-semibold bg-indigo-200 text-indigo-600 px-1.5 py-1 rounded">
+        <code class="bg-indigo-100 text-indigo-700 px-1.5 py-1 rounded">
           {{ $route.path }}
         </code>
         não foi encontrada.
       </p>
       <nuxt-link
         to="/"
-        class="border border-indigo-300 py-6 px-4 bg-indigo-200 rounded text-indigo-600 font-semibold underline">
+        class="border border-indigo-200 py-6 px-4 bg-indigo-100 rounded text-indigo-700 underline">
         Volte à página inicial
       </nuxt-link>
     </shared-container>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -47,7 +47,7 @@ export default defineNuxtConfig({
         {
           rel: "prefetch",
           as: "style",
-          href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap",
+          href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap",
         },
         {
           rel: "prefetch",
