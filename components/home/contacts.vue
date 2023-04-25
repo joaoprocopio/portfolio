106bd836221c6838b2ee0edf1a89a929cac59472
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-04-25 14:56:07 -0300

    tela de contatos

--- components/home/contacts.vue
+++ components/home/contacts.vue
@@ -1,3 +1,12 @@
 <template>
-  <app-container></app-container>
+  <app-container
+    class="bg-emerald-700 text-white"
+    content-class="py-16 text-center sm:text-left">
+    <h2 class="font-semibold">Precisa de um Engenheiro de Software?</h2>
+    <ui-link
+      class="font-bold text-2xl"
+      to="mailto:joaovitorcprocopio@gmail.com">
+      Vamos trabalhar juntos!
+    </ui-link>
+  </app-container>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <lazy-home-introduction id="introdução" class="pt-12" />
-  <lazy-home-about id="sobre" class="pt-12" />
-  <lazy-home-projects id="projetos" class="pt-12" />
-  <lazy-home-contacts id="contato" class="pt-12" />
+  <lazy-home-introduction id="introdução" class="py-12" />
+  <lazy-home-about id="sobre" class="py-12" />
+  <lazy-home-projects id="projetos" class="py-12" />
+  <lazy-home-contacts id="contato" class="py-12" />
 </template>
