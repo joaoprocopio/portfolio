4be97f26b7f596beb5fae65ba805e25e0474bd37
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-04-25 16:50:56 -0300

    refactor ui card

--- components/ui/card.vue
+++ components/ui/card.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="px-4 py-8 rounded-lg">
-    <slot />
+  <div>
+    <div
+      class="px-4 py-8 rounded-lg"
+      :class="{
+        [$props.contentClass]: true,
+      }">
+      <slot />
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+  interface Props {
+    contentClass?: string
+  }
+</script>
+
+<script setup lang="ts">
+  const $props = withDefaults(defineProps<Props>(), {
+    contentClass: "",
+  })
+</script>
--- error.vue
+++ error.vue
@@ -5,7 +5,7 @@
       class="bg-emerald-700"
       content-class="h-safe-screen py-8 flex flex-col items-center justify-center">
       <ui-card
-        class="bg-white flex flex-col items-center justify-center text-center">
+        content-class="bg-white flex flex-col items-center justify-center text-center">
         <h1 class="font-bold text-emerald-700 pb-2">Desculpe</h1>
         <h2 class="text-2xl font-bold pb-2">Ocorreu um erro</h2>
         <p class="text-neutral-500 pb-8 w-48">
