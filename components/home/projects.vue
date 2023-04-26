b68ff9767b1c6f0b866b712beac5734813531a84
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-04-26 13:36:58 -0300

    agr ta exibindo mesmo, antes era mentirinha

--- components/home/projects.vue
+++ components/home/projects.vue
@@ -7,23 +7,25 @@
     <section class="flex flex-col gap-4">
       <template v-for="(project, id) in projects" :key="id">
         <lazy-ui-card
-          content-class="grid grid-rows-2 sm:grid-rows-none sm:grid-cols-2"
-          padding="zero">
+          padding="zero"
+          content-class="w-full h-96 grid grid-rows-2 sm:grid-rows-none sm:grid-cols-2 sm:h-64">
+          <nuxt-img
+            class="w-full h-full object-cover"
+            :src="project.src"
+            :alt="project.alt"
+            quality="80"
+            width="400"
+            loading="lazy" />
           <div
-            class="bg-center bg-cover"
-            :style="{
-              'background-image': src(project.src),
-            }" />
-          <div
-            class="bg-emerald-100 text-emerald-700 flex flex-col gap-8 justify-between px-4 py-4 sm:py-8">
+            class="w-full h-full bg-emerald-100 text-emerald-700 flex flex-col justify-between px-4 py-4 sm:py-8">
             <div>
               <h2 class="text-2xl font-bold">{{ project.title }}</h2>
               <h3 class="text-lg font-medium">{{ project.subtitle }}</h3>
             </div>
             <ui-button
               :to="project.href"
-              target="_blank"
               block
+              target="_blank"
               class="text-emerald-100"
               variant="default">
               Veja o código no GitHub
@@ -39,35 +41,34 @@
   interface Project {
     src: string
     href: string
+    alt: string
     title: string
     subtitle: string
   }
 </script>
 
 <script setup lang="ts">
-  const $image = useImage()
-
   const projects: Ref<Array<Project>> = ref([
     {
       src: "images/black-cat.webp",
       href: "https://github.com/joaoprocopio/djavue",
+      alt: "gato preto em cima de uma pedra",
       title: "Djavue",
       subtitle: "Template full-stack utilizando Django, Vue e Postgres",
     },
     {
       src: "images/wordle.webp",
       href: "https://github.com/joaoprocopio/wordle",
+      alt: "celular com o jogo wordle",
       title: "Wordle",
       subtitle: "Implementação do Wordle em Vue.js",
     },
     {
       src: "images/game-of-life.webp",
       href: "https://github.com/joaoprocopio/life",
+      alt: "blocos formando a palavra life",
       title: "Life",
       subtitle: "Implementação de Game of Life em Vue.js",
     },
   ])
-
-  const src = (src: string): string =>
-    `url(${$image(src, { quality: 80, width: 500 })})`
 </script>
