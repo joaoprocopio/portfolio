a453467d98b2d716a18e7017f717d00163dc37dd
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-04-26 13:55:40 -0300

    refactor footer

--- components/app/footer.vue
+++ components/app/footer.vue
@@ -29,34 +29,38 @@
       </ul>
     </div>
     <div class="grid grid-cols-2 gap-8 items-center">
-      <ul class="flex flex-col gap-2">
+      <div class="flex flex-col gap-2">
         <h2 class="font-medium">Me encontre no</h2>
-        <li>
-          <ui-link
-            class="font-bold"
-            target="_blank"
-            to="https://www.linkedin.com/in/joao-procopio/">
-            LinkedIn
-          </ui-link>
-        </li>
-        <li>
-          <ui-link
-            class="font-bold"
-            target="_blank"
-            to="https://github.com/joaoprocopio/">
-            GitHub
-          </ui-link>
-        </li>
-      </ul>
-      <ul class="flex flex-col gap-2">
+        <ul class="flex flex-col gap-2">
+          <li>
+            <ui-link
+              class="font-bold"
+              target="_blank"
+              to="https://www.linkedin.com/in/joao-procopio/">
+              LinkedIn
+            </ui-link>
+          </li>
+          <li>
+            <ui-link
+              class="font-bold"
+              target="_blank"
+              to="https://github.com/joaoprocopio/">
+              GitHub
+            </ui-link>
+          </li>
+        </ul>
+      </div>
+      <div class="flex flex-col gap-2">
         <h2 class="font-medium">Explore</h2>
-        <li>
-          <ui-link class="font-bold" to="/">Portfólio</ui-link>
-        </li>
-        <li>
-          <ui-link class="font-bold" to="/blog">Blog</ui-link>
-        </li>
-      </ul>
+        <ul class="flex flex-col gap-2">
+          <li>
+            <ui-link class="font-bold" to="/">Portfólio</ui-link>
+          </li>
+          <li>
+            <ui-link class="font-bold" to="/blog">Blog</ui-link>
+          </li>
+        </ul>
+      </div>
       <ui-link to="/" class="text-emerald-100">
         <icon-logo class="h-8 w-8" />
       </ui-link>
