5adcf5a2b380723539bc215334c6e0e1b68f8eb1
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-04-26 14:34:48 -0300

    fix error page

--- error.vue
+++ error.vue
@@ -11,7 +11,11 @@
         <h2 class="text-2xl font-bold pb-2">Ocorreu um erro</h2>
         <p class="text-neutral-500 pb-8 w-48">
           A página
-          <ui-code>{{ path }}</ui-code>
+          <ui-code>{{
+            $route.fullPath.length > 8
+              ? $route.fullPath.slice(0, 5) + "..."
+              : $route.fullPath
+          }}</ui-code>
           não foi encontrada.
         </p>
         <ui-button variant="tonal" padding="large" to="/" @click="handleError">
@@ -22,32 +26,10 @@
   </nuxt-layout>
 </template>
 
-<script lang="ts">
-  interface Error {
-    url: string
-    statusCode: string
-    statusMessage: string
-    message: string
-    stack: string
-  }
-
-  interface Props {
-    error: Error
-  }
-</script>
-
 <script setup lang="ts">
   useHead({
     title: "Página não encontrada",
   })
-
-  const path = computed(() =>
-    $props.error?.url.length > 8
-      ? $props.error?.url.slice(0, 5) + "..."
-      : $props.error?.url
-  )
-
-  const $props = defineProps<Props>()
 
   const handleError = () => clearError({ redirect: "/" })
 </script>
